2ddb16c30f1d2507ad7c15a8f44b65477060d068
Tejaswinipr <67628985+Tejaswinipr@users.noreply.github.com>
2023-01-12 17:31:47 +0530

    Create Project.cs

---
+++ Project.cs
@@ -0,0 +1,33 @@
+Abstraction
+
+using System;
+abstract class Human{
+    public abstract void food();
+    public void eat(){
+        Console.WriteLine("Humans eat food to survive.\n");
+    }
+}
+class Children : Human
+{
+    public override void food(){
+        Console.WriteLine("Children eat light food.");
+    }
+}
+class Adult : Human
+{
+    public override void food()
+    {
+        Console.WriteLine("Adults eat protien rich food");
+    }
+}
+public class Project
+{
+    public static void Main(string[] args)
+    {
+        Children c=new Children();
+        Adult a=new Adult();
+        a.eat();
+        c.food();
+        a.food();
+    }
+}
