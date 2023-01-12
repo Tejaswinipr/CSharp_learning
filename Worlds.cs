53f243429ae136a8955b7cf13eedbbe5282e3b24
Tejaswinipr <67628985+Tejaswinipr@users.noreply.github.com>
2023-01-12 17:25:16 +0530

    Create Worlds.cs

---
+++ Worlds.cs
@@ -0,0 +1,21 @@
+#Properties get and set method
+
+using System;
+
+class Worlds
+{
+    private string name;
+    public string Name{
+        get{ return name;}
+        set{name=value;}
+    }
+}
+class Test{
+public static void Main(string[] args)
+{
+    Worlds w=new Worlds();
+    w.Name="Tejaswini";
+    Console.WriteLine (w.Name);
+}
+}
+
