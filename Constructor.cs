10f02c215622c139e6680926baf68c9c33f3b1dc
Tejaswinipr <67628985+Tejaswinipr@users.noreply.github.com>
2023-01-12 17:26:37 +0530

    Create Constructor.cs

---
+++ Constructor.cs
@@ -0,0 +1,24 @@
+Constructors
+
+
+using System;
+
+class Constructor
+{
+    public string name;
+    public string dept;
+    public int rollNo;
+    public World(string studName, string studDept, int studRollNo){
+        name=studName;
+        dept=studDept;
+        rollNo=studRollNo;
+    }
+}
+class Test{
+public static void Main(string[] args)
+{
+    Constructor w=new Constructor("Tejaswini","CSE",361);
+    Console.WriteLine (w.name+" "+w.dept+" "+w.rollNo);
+}
+}
+
