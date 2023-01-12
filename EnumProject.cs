7b22bd2f896fe562944a88ee72989f0622e53c9e
Tejaswinipr <67628985+Tejaswinipr@users.noreply.github.com>
2023-01-12 17:35:18 +0530

    Create EnumProject.cs

---
+++ EnumProject.cs
@@ -0,0 +1,24 @@
+Enum
+
+
+using System;
+
+public class EnumProject
+{
+    enum weekdays{
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday=10,
+        Thursday,
+        Friday,
+        Saturday
+    }
+    public static void Main(string[] args)
+    {
+    weekdays day=weekdays.Wednesday;
+    Console.WriteLine(day);
+    int num=(int)weekdays.Thursday;
+    Console.WriteLine(num);
+    }
+}
