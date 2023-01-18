41c8ae1ae3c6c9af62f80a71040daab502deb7fe
Tejaswinipr <67628985+Tejaswinipr@users.noreply.github.com>
2023-01-18 15:20:33 +0530

    Update LinqNumGreater.cs

--- LinqNumGreater.cs
+++ LinqNumGreater.cs
@@ -1,18 +1,25 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public class LinqNumGreater
 {
     public static void Main(string[] args)
     {
-        int[] arr={55, 200, 740, 76, 230, 482, 95};
-        var nQuery=from num in arr
-                    where num>80
-                    select num;
+        List<int> arr=new List<int>();
+        arr.Add(55);
+        arr.Add(200);
+        arr.Add(740);
+        arr.Add(76);
+        arr.Add(230);
+        arr.Add(482);
+        arr.Add(95);
+        List<int> queryList=arr.FindAll(a => a > 80 ? true : false);
+        
         Console.WriteLine("The Numbers greater than 80 are ");
-        foreach (var i in nQuery)
+        foreach (var i in queryList)
         {
-          Console.WriteLine("{0} ",i);  
+          Console.WriteLine(i);  
         }
     }
 }
