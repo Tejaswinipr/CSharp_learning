17aa5f15bc3b46fc4ef772af5afbcc5f6dfd37f6
Tejaswinipr <67628985+Tejaswinipr@users.noreply.github.com>
2023-01-23 08:15:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Logger.WriteLog("The End");
+                
+            }
+            catch (Exception exception)
+            {
+                Logger.WriteLog(exception.Message);
+            }
+        }
+    }
+}
